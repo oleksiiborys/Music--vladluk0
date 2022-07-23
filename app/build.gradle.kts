280011ca2c92aea3c10400fdfbf68d041e8edf64
vlad <vladlukiyanchukoo@gmail.com>
2022-07-24 01:36:05 +0300

    build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,52 +58,23 @@ dependencies {
     api(":authentification")
     api(":module-injector")
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha14")
-    implementation("androidx.compose.material3:material3-window-size-class:1.0.0-alpha14")
-    implementation("androidx.activity:activity-compose:1.5.0")
-    implementation("com.google.firebase:firebase-auth-ktx:21.0.6")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.0-alpha01")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0-alpha01")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.0-alpha01")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha03")
-
     implementation(libs.bundles.compose)
-    //lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
+    implementation(libs.bundles.composeMaterial)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.bundles.dagger)
+    implementation(libs.androidXCore)
+    implementation(libs.androidXLifecycle)
+    implementation(libs.composeActivity)
+    implementation(libs.composeConstraint)
+    implementation(libs.composeLiveData)
+    implementation(libs.composeCoil)
+    implementation(libs.composeNavigation)
+    implementation(libs.accompanistSwipeRefresh)
+    implementation(libs.firebaseBom)
+    implementation(libs.firebaseAnalytics)
+    implementation(libs.firebaseAuth)
+    implementation(libs.firebaseFireStore)
+    implementation(libs.coroutinesPlayService)
+    implementation(libs.authPlayService)
 
-    //lifecycle compose
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-alpha01")
-
-    //retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    //dagger
-    implementation("com.google.dagger:dagger:2.42")
-    kapt("com.google.dagger:dagger-compiler:2.42")
-
-    //compose
-    implementation("androidx.compose.runtime:runtime-livedata:1.3.0-alpha01")
-
-    //coil
-    implementation("io.coil-kt:coil-compose:2.1.0")
-
-    //navigation
-    implementation("androidx.navigation:navigation-compose:2.5.0")
-
-    //
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.24.11-rc")
-
-    //firebase
-    api("com.google.firebase:firebase-bom:30.3.0")
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    //Play Services
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:20.2.0")
-    implementation("com.google.android.gms:play-services-auth:1.6.0")
 }
