5744529137f8889bb508966d044589a8f9a45ad1
vlad <vladlukiyanchukoo@gmail.com>
2022-07-24 17:28:35 +0300

    build

--- app/src/main/java/com/example/music/MainActivity.kt
+++ app/src/main/java/com/example/music/MainActivity.kt
@@ -7,17 +7,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import com.example.music.di.DaggerApplicationComponent
-import com.example.music.ui.screen.add_artist.AddArtist
-import com.example.music.ui.screen.add_artist.AddArtistVM
-
 import com.example.music.ui.theme.MusicTheme
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
 
